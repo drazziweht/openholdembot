72c29c7f393cd3721955d842c675b2ddb55664f4
TheHighFish <thehighfish@googlemqail.com>
2016-02-03 22:30:28 +0100

    Added icon definition for SH

--- OpenHoldem/CSymbolEnginePokerTrackerIcon.cpp
+++ OpenHoldem/CSymbolEnginePokerTrackerIcon.cpp
@@ -15,6 +15,7 @@
 #include "stdafx.h"
 #include "CSymbolEnginePokerTrackerIcon.h"
 
+#include "CSymbolEngineActiveDealtPlaying.h"
 #include "CSymbolEnginePokerTracker.h"
 #include "CTableState.h"
 #include "StringFunctions.h"
@@ -47,10 +48,12 @@ typedef struct {
 
 const int kMaxNumberOfRelevantStatsPerIconType = 4;
 
-const t_poker_tracker_symbol_range
-  poker_tracker_symbol_range_full_ring[kNumberOfPokerTrackerIcons][kMaxNumberOfRelevantStatsPerIconType] = {
+typedef t_poker_tracker_symbol_range t_icon_definition_table
+  [kNumberOfPokerTrackerIcons][kMaxNumberOfRelevantStatsPerIconType];
+
+const t_icon_definition_table icon_definition_table_full_ring = {
   // 2	LP (Fishy)	9	
-  {{"VP$IP", 0.35, 1}, {"Total AF", 0.01, 1.3}},
+  {{"VP$IP", 0.35, 1}, {"Total AF", 0.00, 1.3}},
   // 3	X-Loose (Vp$iP 55%+)	1	
   {{"VP$IP", 0.55, 1}},
   // 4	Showdown Muppet	3	
@@ -62,7 +65,7 @@ const t_poker_tracker_symbol_range
   // 8	sLA (Gambler)	6	
   {{"VP$IP", 0.22, 0.35}, {"Flop AF", 1.8, 123}, {"Turn AF", 0.1, 123}, {"River AF", 0.1, 123}},
   // 9	TP (Rock)	10	
-  {{"VP$IP", 0, 0.22}, {"Total AF", 0.01, 1.3}},
+  {{"VP$IP", 0, 0.22}, {"Total AF", 0, 1.3}},
   // 7	Ultra Aggressor	4	
   {{"Flop AF", 2.75, 123}, {"Turn AF", 1.7, 123}, {"River AF", 1.7, 123}},
   // 10	LAG (Maniac)	7	
@@ -72,7 +75,7 @@ const t_poker_tracker_symbol_range
   // 12	Calling station	2	
   {{"VP$IP", 0.22, 1}, {"WTSD", 0.30, 1}, {"Total AF", 0, 1}},
   // 13	sLP	11	
-  {{"VP$IP", 0.22, 0.35}, {"Total AF", 0.01, 1.3}},
+  {{"VP$IP", 0.22, 0.35}, {"Total AF", 0, 1.3}},
   // 14	L N/unknown 	14	
   {{"VP$IP", 0.35, 1}},
   // 15	TAG	8	
@@ -81,27 +84,44 @@ const t_poker_tracker_symbol_range
   {{"VP$IP", 0, 1}}
 };
 
-/* short handed
-2	LP (Fishy)	9	"VP$IP","between","46","99"|"Total AF","between",".01","1.65"|
-3	X-Loose (Vp$iP 63%+)	1	"VP$IP","between","63","99"|
-4	Showdown Muppet	3	"W$SD","between","1","33"|"WTSD",">","20",""|
-5	T N/unknown	12	"VP$IP","<","33",""|
-6	WT (Weak Tight)	5	"WTSD","between","1","14"|
-8	sLA (Gambler)	6	"VP$IP","between","33","46"|"Flop AF",">=","2",""|"Turn AF",">",".1",""|"River AF",">",".1",""|
-9	TP (Rock)	10	"VP$IP","<","33",""|"Total AF","between",".01","1.3"|
-7	Ultra Aggressor	4	"Flop AF",">=","2.75",""|"Turn AF",">=","2",""|"River AF",">=","2",""|
-10	LAG (Maniac)	7	"VP$IP","between","46","99"|"Flop AF",">=","2",""|"Turn AF",">",".1",""|"River AF",">",".1",""|
-11	sL N/unknown	13	"VP$IP","between","33","46"|
-12	Calling station	2	"VP$IP",">","33",""|"WTSD","between","33","99"|"Total AF","between","0.01","1.65"|
-13	sLP	11	"VP$IP","between","33","46"|"Total AF","between",".01","1.65"|
-14	L N/unknown 	14	"VP$IP","between","46","99"|
-15	TAG	8	"VP$IP","<","33",""|"Flop AF",">=","2",""|"Turn AF",">",".1",""|"River AF",">",".1",""|
-*/
+const t_icon_definition_table icon_definition_table_short_handed = {
+  // 2	LP (Fishy)	9	
+  {{"VP$IP", 0.46, 1}, {"Total AF", 0.00, 1.65}},
+  // 3	X-Loose (Vp$iP 63%+)	1	
+  {{"VP$IP", 0.53, 1}},
+  // 4	Showdown Muppet	3	
+  {{"W$SD", 0, 0.33}, {"WTSD", 0.20, 1}},
+  // 5	T N/unknown	12	
+  {{"VP$IP", 0, 0.33}},
+  // 6	WT (Weak Tight)	5	
+  {{"WTSD", 0, 0.22}},	
+  // 8	sLA (Gambler)	6	
+  {{"VP$IP", 0.22, 0.33}, {"Flop AF", 2, 123}, {"Turn AF", 0.1, 123}, {"River AF", 0.1, 123}},
+  // 9	TP (Rock)	10	
+  {{"VP$IP", 0, 0.33}, {"Total AF", 0, 1.3}},
+  // 7	Ultra Aggressor	4	
+  {{"Flop AF", 2.75, 123}, {"Turn AF", 2, 123}, {"River AF", 2, 123}},
+  // 10	LAG (Maniac)	7	
+  {{"$VPIP", 0.46, 1}, {"Flop AF", 2, 123}, {"Turn AF", 0.1, 123}, {"River AF", 0.1, 123}},
+  // 11	sL N/unknown	13	
+  {{"VP$IP", 0.23, 0.46}},
+  // 12	Calling station	2	
+  {{"VP$IP", 0.33, 1}, {"WTSD", 0.33, 1}, {"Total AF", 0, 1.65}},
+  // 13	sLP	11	
+  {{"VP$IP", 0.33, 0.46}, {"Total AF", 0, 1.65}},
+  // 14	L N/unknown 	14	
+  {{"VP$IP", 0.46, 1}},
+  // 15	TAG	8	
+  {{"VP$IP", 0, 0.33}, {"Flop AF", 2, 123}, {"Turn AF", 0.1, 123}, {"River AF", 0.1, 123}},
+  // pt_Unknown,
+  {{"VP$IP", 0, 1}}
+};
 
 CSymbolEnginePokerTrackerIcon::CSymbolEnginePokerTrackerIcon() {
 	// The values of some symbol-engines depend on other engines.
 	// As the engines get later called in the order of initialization
 	// we assure correct ordering by checking if they are initialized.
+  assert(p_symbol_engine_active_dealt_playing != NULL);
 	assert(p_symbol_engine_pokertracker != NULL); 
   ClearAllStats();
 }
@@ -138,9 +158,26 @@ void CSymbolEnginePokerTrackerIcon::ClearAllStats() {
 }
 
 bool CSymbolEnginePokerTrackerIcon::PlayerMatchesIconDefinition(int chair, int icon) {
+  const t_icon_definition_table *icon_definition_table;
+  // Selecting the proper icon-definition-table
+  // that covers the stats of the game-type we usually play.
+  // We can't use nchairs here, because the same tablemap 
+  // might be usable for FR / SH / HU.
+  // We can't use nplayersdealt either, because we might usually play
+  // FR and all stats of our opponent are from FR-games,
+  // but now as an exception we play SH / HU.
+  // Therefore we take maxnplayersdealt.
+  // FR: 7..10 handed.
+  // SH: 2..6 handed.
+  // HU: no special icon definitins found so far
+  if (p_symbol_engine_active_dealt_playing->maxnplayersdealt() >= 7) {
+    icon_definition_table = &icon_definition_table_full_ring;
+  } else {
+    icon_definition_table = &icon_definition_table_short_handed;
+  }
   CString pt_symbol_with_chair;
   for (int i=0; i<kMaxNumberOfRelevantStatsPerIconType; ++i) {
-    if (poker_tracker_symbol_range_full_ring[icon][i].pt_symbol == "") {
+    if (icon_definition_table[icon][i]->pt_symbol == "") {
       // No mismatch found so far, but no more stats to compare
       // This is a success
       return true;
@@ -148,7 +185,7 @@ bool CSymbolEnginePokerTrackerIcon::PlayerMatchesIconDefinition(int chair, int i
     // Lookup stat
     double pt_value = kUndefined;
     pt_symbol_with_chair.Format("%s%d",
-      poker_tracker_symbol_range_full_ring[icon][i].pt_symbol, chair);
+      icon_definition_table[icon][i]->pt_symbol, chair);
     bool success = p_symbol_engine_pokertracker->EvaluateSymbol(
       pt_symbol_with_chair, &pt_value, true); //!!!!!
     if (!success) {
@@ -160,10 +197,10 @@ bool CSymbolEnginePokerTrackerIcon::PlayerMatchesIconDefinition(int chair, int i
       return false;
     }
     // Check range
-    if ((pt_value < poker_tracker_symbol_range_full_ring[icon][i].min_value)) {
+    if ((pt_value < icon_definition_table[icon][i]->min_value)) {
       return false;
     }
-    if ((pt_value > poker_tracker_symbol_range_full_ring[icon][i].max_value)) {
+    if ((pt_value > icon_definition_table[icon][i]->max_value)) {
       return false;
     }
   }
