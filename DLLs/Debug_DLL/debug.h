bb5f7b382236f297e1d7e2c7a4ab89d0903e256f
TheHighFish <thehighfish@googlemail.com>
2017-10-30 15:57:45 +0100

    name-mangling for debug-DLL

--- DLLs/Debug_DLL/debug.h
+++ DLLs/Debug_DLL/debug.h
@@ -15,7 +15,7 @@
 #ifdef DEBUG_DLL_EXPORTS
 #define DEBUG_DLL_API extern "C" __declspec(dllexport)
 #else
-#define DEBUG_DLL_API __declspec(dllimport)
+#define DEBUG_DLL_API extern "C" __declspec(dllimport)
 #endif
 
 #include <stdarg.h>
