a6d52afc7741dba726f681b42a4024c2422dae77
TheHighFish <thehighfish@googlemail.com>
2018-02-07 15:18:53 +0100

    restructuring solution

--- OpenHoldem/MainFrm.h
+++ OpenHoldem/MainFrm.h
@@ -1,5 +1,16 @@
-#ifndef INC_MAINFRM_H
-#define INC_MAINFRM_H
+#pragma once
+//******************************************************************************
+//
+// This file is part of the OpenHoldem project
+//    Source code:           https://github.com/OpenHoldem/openholdembot/
+//    Forums:                http://www.maxinmontreal.com/forums/index.php
+//    Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//******************************************************************************
+//
+// Purpose:
+//
+//******************************************************************************
 
 #include "..\CTablemap\CTablemap.h"
 #include "..\Shared\CCritSec\CCritSec.h"
@@ -92,5 +103,3 @@ BOOL CALLBACK EnumProcTopLevelWindowList(HWND hwnd, LPARAM lparam);
 
 // Accessor function because p_mainframe never gets initialized.
 CMainFrame* PMainframe();
-
-#endif /* INC_MAINFRM_H */
